64f9ab3ab042e658de37403001af595d988705cb
Jonathan Bomgardner <jonlb@comcast.net>
2010-12-25 16:14:56 -0800

    remove vendors/loader

--- classes/jx/loader.php
+++ classes/jx/loader.php
@@ -1,7 +1,31 @@
 <?php
 
 
-class Jx_Loader {
+class Jx_Loader implements Jx_Module_Interface {
+
+    private static $_version = '0.1';
+
+    public static function deactivate() {
+        // TODO: Implement deactivate() method.
+    }
+
+    public static function activate() {
+        // TODO: Implement activate() method.
+    }
+
+    public static function uninstall() {
+        // TODO: Implement uninstall() method.
+    }
+
+    public static function install() {
+        // TODO: Implement install() method.
+    }
+
+    public static function getVersion() {
+        return self::$_version;
+    }
+
+
 
     public static function uri(array $options) {
         $uriParts = array();
